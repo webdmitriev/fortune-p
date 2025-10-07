759b27fbc55a4ac56c5e808f85d89de06784ca44
webdmitriev <webdmitriev@gmail.com>
2025-10-07 19:38:14 +0300

    Optimize js for rating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // rating
-  document.querySelectorAll('.rating-stars').forEach((el, index) => {
-    const rating = parseFloat(el.dataset.rating) || 0;
-    const full = Math.floor(rating);
-    const half = rating % 1 >= 0.5 ? 1 : 0;
-    const empty = 5 - full - half;
+  function initRatingStars() {
 
-    const starPath = 'M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z';
+    if (window.innerWidth <= 768) return;
 
-    let html = '';
+    document.querySelectorAll('.rating-stars').forEach((el, index) => {
+      const rating = parseFloat(el.dataset.rating) || 0;
+      const full = Math.floor(rating);
+      const half = rating % 1 >= 0.5 ? 1 : 0;
+      const empty = 5 - full - half;
 
-    // Полные звезды
-    for (let i = 0; i < full; i++) {
-      html += `<svg class="full" viewBox="0 0 24 24"><path d="${starPath}"/></svg>`;
-    }
+      const starPath = 'M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z';
 
-    // Половинка
-    if (half) {
-      const gradId = `half-grad-${index}`;
-      html += `
-      <svg class="half" viewBox="0 0 24 24">
-        <defs>
-          <linearGradient id="${gradId}">
-            <stop offset="50%" stop-color="#f3bb44"/>
-            <stop offset="50%" stop-color="#C7C7C7"/>
-          </linearGradient>
-        </defs>
-        <path d="${starPath}" fill="url(#${gradId})"/>
-      </svg>
-    `;
-    }
+      let html = '';
 
-    // Пустые
-    for (let i = 0; i < empty; i++) {
-      html += `<svg class="empty" viewBox="0 0 24 24"><path d="${starPath}"/></svg>`;
-    }
+      for (let i = 0; i < full; i++) {
+        html += `<svg class="full" viewBox="0 0 24 24"><path d="${starPath}"/></svg>`;
+      }
 
-    el.innerHTML = html + ` <p>${rating}</p>`;
-  });
+      if (half) {
+        const gradId = `half-grad-${index}`;
+        html += `
+        <svg class="half" viewBox="0 0 24 24">
+          <defs>
+            <linearGradient id="${gradId}">
+              <stop offset="50%" stop-color="#f3bb44"/>
+              <stop offset="50%" stop-color="#C7C7C7"/>
+            </linearGradient>
+          </defs>
+          <path d="${starPath}" fill="url(#${gradId})"/>
+        </svg>
+      `;
+      }
+
+      for (let i = 0; i < empty; i++) {
+        html += `<svg class="empty" viewBox="0 0 24 24"><path d="${starPath}"/></svg>`;
+      }
+
+      el.innerHTML = html + ` <p>${rating}</p>`;
+    });
+  }
+
+  document.addEventListener('DOMContentLoaded', initRatingStars);
+  window.addEventListener('resize', initRatingStars);
+
 
 });
